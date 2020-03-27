478224a25b95fd6bc587ad13fa372406c16440e5
Iclipsee <iclipskii@gmail.com>
2020-03-27 19:27:11 +0100

    Latest finished build

--- src/de/MangoleHD/IMLobby/StaticClasses/getScoreboard.java
+++ src/de/MangoleHD/IMLobby/StaticClasses/getScoreboard.java
@@ -5,6 +5,7 @@ import de.MangoleHD.IMLobby.Data;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 
@@ -13,9 +14,28 @@ import java.util.HashMap;
 public class getScoreboard {
     public static int ticks = 0;
     public static int animation = 0;
-    public static HashMap<Player, ScoreboardSign> boards = new HashMap<>();
+    public static HashMap<Player, ScoreboardSign> boards;
 
     public static void createScoreboard(Player p) {
+        int max = 15;
+        /*
+        Scoreboard board = p.getScoreboard();
+        Objective obj = board.getObjective("Lobby") != null ? board.getObjective("Lobby") : board.registerNewObjective("Lobby", "aaa", "§5§lIM§r§fLobby");
+        if(obj.getDisplaySlot() != DisplaySlot.SIDEBAR){
+            obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+        }
+        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+        obj.getScore("§6" + p.getName()).setScore(8);
+        obj.getScore("" + ChatColor.BLACK).setScore(7);
+        obj.getScore("§2Buon Giorno").setScore(6);
+        obj.getScore("" + getAnimation(p, 0) + ChatColor.DARK_BLUE).setScore(5);
+        obj.getScore("" + getAnimation(p, 1) + ChatColor.RED).setScore(4);
+        obj.getScore("" + getAnimation(p, 2) + ChatColor.BLUE).setScore(3);
+        obj.getScore(Data.dsp.get("scoreboard.line.6", p)).setScore(2);
+        obj.getScore(Data.dsp.get("scoreboard.line.7", p)).setScore(1);
+        obj.getScore(Data.dsp.get("scoreboard.line.8", p)).setScore(0);
+        p.setScoreboard(board);
+        */
         ScoreboardSign ss = new ScoreboardSign(p, "§5§lIM§r§fLobby");
         ss.create();
         ss.setLine(0, "§6" + p.getName());
@@ -28,9 +48,24 @@ public class getScoreboard {
         ss.setLine(7, Data.dsp.get("scoreboard.line.7", p));
         ss.setLine(8, Data.dsp.get("scoreboard.line.8", p));
         boards.put(p, ss);
+
     }
 
     public static void updateScoreboard(Player p){
+        /*
+        Scoreboard board = p.getScoreboard();
+        Objective obj = board.getObjective("Lobby") != null ? board.getObjective("Lobby") : board.registerNewObjective("Lobby", "aaa", "§5§lIM§r§fLobby");
+        obj.getScore("§6" + p.getName()).setScore(8);
+        obj.getScore("" + ChatColor.BLACK).setScore(7);
+        obj.getScore("§2Buon Giorno").setScore(6);
+        obj.getScore("" + getAnimation(p, 0) + ChatColor.DARK_BLUE).setScore(5);
+        obj.getScore("" + getAnimation(p, 1) + ChatColor.RED).setScore(4);
+        obj.getScore("" + getAnimation(p, 2) + ChatColor.BLUE).setScore(3);
+        obj.getScore(Data.dsp.get("scoreboard.line.6", p)).setScore(2);
+        obj.getScore(Data.dsp.get("scoreboard.line.7", p)).setScore(1);
+        obj.getScore(Data.dsp.get("scoreboard.line.8", p)).setScore(0);
+        p.setScoreboard(board);
+        */
         ScoreboardSign ss = boards.get(p);
         ss.create();
         ss.setLine(0, "§6" + p.getName());
